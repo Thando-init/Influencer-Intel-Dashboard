a82564a3e6c12c0841b08ad23963ca5dbed68419
Thando Majola <thandomaajola@gmail.com>
2026-03-07 21:09:29 +0000

    Delete tests directory

--- tests/test_client.py
+++
@@ -1,9 +0,0 @@
-from ..src.youtube.client import get_channel_stats, get_recent_videos
-
-channel = "@MrBeast"
-stats = get_channel_stats(channel)
-print(stats)
-
-if stats:
-    videos = get_recent_videos(stats["uploads_playlist_id"], count=5)
-    print(videos)
